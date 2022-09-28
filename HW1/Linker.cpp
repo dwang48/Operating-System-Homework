d8969ce3942ab8eda8eb72ccbc7e18de7df3afa6
Dinghong Wang <michaelwang970403@gmail.com>
2022-09-27 22:33:40 -0500

    update

--- HW1/Linker.cpp
+++ HW1/Linker.cpp
@@ -1,21 +1,65 @@
-#include<iostream>
-#include<stdio.h>
-#include<string.h>
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <vector>
+#include <cctype>
+#include <map>
+#include <algorithm>
+#include <stdio.h>
+#include <string.h>
+
+using namespace std;
+
+
+struct definition_list{
+  int defcount = 0;
+  vector<pair<string,string>> deflist;
+};
+
+struct use_list{
+  int usecount = 0;
+  vector<string> uselist;
+};
+
+struct program_text{
+  int codecount = 0;
+  vector<pair<char,int>> text;
+};
+
+//functions list for the assignment
+void read(char* argv[]);
 
 
 
 
 
-int main ()
-{
-  char str[] ="- This, a sample string.";
-  char * pch;
-  printf ("Splitting string \"%s\" into tokens:\n",str);
-  pch = strtok (str," ,.-");
-  while (pch != NULL)
-  {
-    printf ("%s\n",pch);
-    pch = strtok (NULL, " ,.-");
+
+
+void read(char* argv[]){
+  string line;
+  ifstream input(argv[1]);
+  ifstream output(argv[2]);
+  if(input.is_open()){
+    while(getline(input,line)){
+      cout<<line<<endl;
+    }
+  input.close();
   }
+  else{
+    cout<<"Unable to open file";
+  }
+}
+
+
+
+
+int main (int argc, char* argv[])
+{
+  cout<<argc<<endl;
+  if(argc !=3){
+    cout<< "Both Input file and Out file required."<<endl;
+    exit(EXIT_FAILURE);
+  }
+  read(argv);
   return 0;
 }
