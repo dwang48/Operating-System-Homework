5162b53b4f16621a6f8eb91d568f24e2caa6e5df
dwang48 <michaelwang970403@gmail.com>
2022-11-21 22:52:54 -0500

    init

---
+++ HW3/mmu.cpp
@@ -0,0 +1,50 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <iostream>
+#include <fstream>
+#include <getopt.h>
+#include <iomanip>
+//#include <cstring>
+#include <string>
+#include <list>
+#include <vector>
+#include <queue>
+#include <unordered_map>
+
+using namespace std;
+
+
+typedef struct{
+
+} pte_t;
+
+typedef struct
+{
+};
+
+
+
+
+
+void simulation(){
+
+}
+
+int main(int argc, char* argv[]){
+
+
+//read input
+int c;
+while(true){
+    c = getopt(argc,argv,)
+}
+char* inputfile = argv[optind];
+ifstream in(inputfile);    
+
+
+
+simulation();
+
+return 0;
+
+}
